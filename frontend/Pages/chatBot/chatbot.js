9cdbe2095a16ec079f67a9c31e47eb4c12ee3b7e
Matheus Ursino <Matheusursino008@gmail.com>
2026-07-04 17:42:34 -0300

    atualização da URL da API

--- frontend/Pages/chatBot/chatbot.js
+++ frontend/Pages/chatBot/chatbot.js
@@ -9,7 +9,7 @@
    CONFIGURAÇÃO
    URL do servidor backend que faz proxy para a API do Gemini
    -------------------------------- */
-const API_URL = "http://localhost:3000/api/analisar";
+const API_URL = "http://plataforma-verifica-ai-c64h.vercel.app/api/analisar";
 
 
 /* --------------------------------
